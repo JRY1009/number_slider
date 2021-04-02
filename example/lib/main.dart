0f88ed9499b1ec8e6cff9792657f889f54437054
zj <jerryzhou1009@163.com>
2021-04-02 16:24:53 +0800

    example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   Timer _timer;
 
   Random rng = Random();
@@ -24,9 +23,36 @@ class _MyAppState extends State<MyApp> {
   NumberSliderController nsController2 = NumberSliderController();
   NumberSliderController nsController3 = NumberSliderController();
 
-  var numList1 = ['32133.32', '32146.67', '32146.67', '53146.44', '72146.12', '32045.42', '12143.61', '32146.09'];
-  var numList2 = ['-2,133.32', '-3,246.67', '-2,146.67', '-3,146.44', '-2,146.12', '-2,045.42', '-2,143.61', '-2,146.09'];
-  var numList3 = ['32,133.32', '32,146.67', '2,146.67', '3,146.44', '7,146.12', '32,045.42', '12,143.61', '32,146.09'];
+  var numList1 = [
+    '32133.32',
+    '32146.67',
+    '32146.67',
+    '53146.44',
+    '72146.12',
+    '32045.42',
+    '12143.61',
+    '32146.09'
+  ];
+  var numList2 = [
+    '-2,133.32',
+    '-3,246.67',
+    '-2,146.67',
+    '-3,146.44',
+    '-2,146.12',
+    '-2,045.42',
+    '-2,143.61',
+    '-2,146.09'
+  ];
+  var numList3 = [
+    '32,133.32',
+    '32,146.67',
+    '2,146.67',
+    '3,146.44',
+    '7,146.12',
+    '32,045.42',
+    '12,143.61',
+    '32,146.09'
+  ];
 
   @override
   void initState() {
@@ -65,9 +91,7 @@ class _MyAppState extends State<MyApp> {
                     color: Color(0xFFEC3944),
                     fontWeight: FontWeight.w600,
                     fontSize: 22,
-                    height: 1.0
-                )
-            ),
+                    height: 1.0)),
             SizedBox(height: 20),
             NumberSlider(
                 controller: nsController2,
@@ -76,9 +100,7 @@ class _MyAppState extends State<MyApp> {
                     color: Color(0xFF22C29B),
                     fontWeight: FontWeight.w600,
                     fontSize: 22,
-                    height: 1.0
-                )
-            ),
+                    height: 1.0)),
             SizedBox(height: 20),
             NumberSlider(
                 controller: nsController3,
@@ -87,9 +109,7 @@ class _MyAppState extends State<MyApp> {
                     color: Color(0xff333A50),
                     fontWeight: FontWeight.w600,
                     fontSize: 22,
-                    height: 1.0
-                )
-            ),
+                    height: 1.0)),
           ],
         ),
       ),
